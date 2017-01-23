bf704eac06d4652d3a4cae1a05b051c189b14949
François Pottier <francois.pottier@inria.fr>
2017-01-23 15:21:51 +0100

    Makefiles for the examples.

---
+++ examples/proofs/Examples.v
@@ -0,0 +1,8 @@
+Set Implicit Arguments.
+Require Import LibTactics.
+(* Load the CFML library, with time credits. *)
+Require Import CFLib.
+Require Pervasives_ml.
+Require Import Pervasives_proof.
+Require Import CFLibCreditsNat.
+Require Import Max_subsequence_sum_ml.
