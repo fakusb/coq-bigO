2502c9f4cf6f38944c513de605dcc3b328c9a2cf
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2017-10-19 21:37:16 +0200

    More on the same case study

--- examples/proofs/Multivar_specialize_proof.v
+++ examples/proofs/Multivar_specialize_proof.v
@@ -69,11 +69,9 @@ Lemma dominated_cst_limit_a2 A B c g :
   dominated (asymproduct_filterType A B) (fun '(_,_) => c) g.
 Admitted.
 
+(*----------------------------------------------------------------------------*)
 
-Notation product_order_order :=
-  (product_filterType Z_filterType Z_filterType) (only parsing).
-
-(* Does not work: see proofs below *)
+(* Symmetric product "everywhere x order". Does not work: see proofs below *)
 Notation product_everywhere_order :=
   (product_filterType (everywhere_filterType Z) Z_filterType) (only parsing).
 
@@ -106,7 +104,9 @@ Proof.
   math.
 Qed.
 
-(* Better, but still does not work.
+(*----------------------------------------------------------------------------*)
+
+(* Asymmetric product "everywhere x order". Better, but still does not work.
    dominated 1 (m * n + n)  does not hold since one could chose m = -1.
 *)
 Notation asymproduct_everywhere_order :=
@@ -126,6 +126,90 @@ Qed.
 Lemma positive_inhab : exists x, 0 <= x.
 Proof. exists 0. math. Qed.
 
+(*----------------------------------------------------------------------------*)
+
+(* Symmetric product restricting 0 <= m : Works *)
+Notation product_positive_order :=
+  (product_filterType (on_filterType positive_inhab) Z_filterType) (only parsing).
+
+Lemma product_positive_order_limit :
+  limit product_positive_order Z_filterType
+    (fun '(m,n) => n * m + n).
+Proof.
+  rewrite limitP. intros P UP.
+  rewrite ZP_ultimately with (cond := fun x => 0 <= x) in UP
+    by (apply ultimately_ge_Z).
+  destruct UP as (n0 &_N0 & H).
+  rewrite productP. exists (fun x => 0 <= x) (fun x => n0 <= x). splits.
+  rewrite~ onP. apply ultimately_ge_Z.
+   simpl. intros. apply H. math_nia.
+Qed.
+
+Lemma g_spec :
+  specO
+    product_positive_order
+    eq (* dummy *)
+    (fun cost =>
+      (forall m n,
+         0 <= n -> 0 <= m -> (* FIXME (need more xfor lemmas) *)
+         app g [(m, n)]
+         PRE (\$ cost (m, n))
+         POST (fun (_:unit) => \[])))
+    (fun '(m, n) => n * m + n).
+Proof.
+  xspecO.
+  xcf. xpay. xmatch.
+  xfor_inv (fun (_:int) => \[]). math.
+  { intros i I. apply pay1.
+    xfor_inv (fun (_:int) => \[]). math.
+    intros j J. apply pay1. xret. hsimpl. hsimpl.
+    simpl. clean_max0. rewrite cumulP. rewrite big_const_Z. ring_simplify.
+    apply Z.le_refl.
+    hsimpl.
+  }
+  hsimpl.
+  simpl. rewrite cumulP. rewrite big_const_Z. rewrite~ max0_eq.
+  hide_evars_then ltac:(fun _ => ring_simplify). apply Z.le_refl.
+  hsimpl.
+
+  cleanup_cost.
+  admit.
+
+  apply dominated_sum_distr_2.
+  { apply dominated_max0_2. apply dominated_reflexive. }
+  { apply dominated_cst_limit_2. apply product_positive_order_limit. }
+Qed.
+
+(* TODO: make xapp_spec work with a specO *)
+Hint Extern 1 (RegisterSpec g) => Provide (provide_specO g_spec).
+
+Lemma f_spec :
+  specO
+    Z_filterType
+    eq (* dummy *)
+    (fun cost =>
+      (forall n,
+         0 <= n ->
+         app f [n]
+         PRE (\$ cost n)
+         POST (fun (_:unit) => \[])))
+    (fun n => n).
+Proof.
+  xspecO. xcf. xpay. xapp~.
+  cleanup_cost.
+  admit.
+  { dominated.
+    eapply dominated_comp_eq. applys cost_dominated g_spec.
+    Focus 2. intros. reflexivity.
+    Focus 2. intros. simpl. math.
+    rewrite limitP. simpl. intros P UP. rewrite productP in UP. simpl in UP.
+    destruct UP as (P1 & P2 & UP1 & UP2 & H). rewrite onP in UP1. revert UP2.
+    filter_closed_under_intersection. auto with zarith. }
+Qed.
+
+(*----------------------------------------------------------------------------*)
+
+(* Asymmetric product restricting 0 <= m : Also works *)
 Definition asymproduct_positive_order :=
   (asymproduct_filterType (on_filterType positive_inhab) Z_filterType).
 
@@ -142,7 +226,7 @@ Proof.
   intros. apply H. math_nia.
 Qed.
 
-Lemma g_spec :
+Lemma g_spec' :
   specO
     asymproduct_positive_order
     eq (* dummy *)
@@ -178,9 +262,9 @@ Proof.
 Qed.
 
 (* TODO: make xapp_spec work with a specO *)
-Hint Extern 1 (RegisterSpec g) => Provide (provide_specO g_spec).
+Hint Extern 1 (RegisterSpec g) => Provide (provide_specO g_spec').
 
-Lemma f_spec :
+Lemma f_spec' :
   specO
     Z_filterType
     eq (* dummy *)
@@ -196,7 +280,7 @@ Proof.
   cleanup_cost.
   admit.
   { dominated.
-    eapply dominated_comp_eq. applys cost_dominated g_spec.
+    eapply dominated_comp_eq. applys cost_dominated g_spec'.
     Focus 2. intros. reflexivity.
     Focus 2. intros. simpl. math.
     rewrite limitP. simpl. intros P UP.
@@ -204,6 +288,7 @@ Proof.
     rewrite onP in UP. apply~ UP. }
 Qed.
 
+(*----------------------------------------------------------------------------*)
 
 (* There is also the solution of quantifying m outside the specO... This
    trivially ensures we can instantiate it later.
@@ -211,8 +296,8 @@ Qed.
    It works (we can prove that [forall m, g is O(n)]), but this is basically a
    uni-variate specification now. The O() constant can (and does) depend on m...
 
-   Question: Does using the previous less agressive asymmetrical filter give us
-   additionnal properties?...
+   Question: Does using the proper previous filters give us additionnal
+   properties?...
 *)
 
 Definition product_singleton_order (m : Z) : filterType.
@@ -220,7 +305,7 @@ Definition product_singleton_order (m : Z) : filterType.
   abstract (exists m; reflexivity).
 Defined.
 
-Lemma multivar_specialize_spec' :
+Lemma g_spec'' :
   forall m,
   specO
     (product_singleton_order m)
