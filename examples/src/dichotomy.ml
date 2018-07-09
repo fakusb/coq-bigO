0e220ba9e82d0eaa2e758a2d5d9ff74b1c856d73
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2018-07-09 15:43:45 +0200

    Syntax fixes

--- examples/src/dichotomy.ml
+++ examples/src/dichotomy.ml
@@ -1,14 +1,10 @@
-(* Pour prouver la récursion : si |j-i| < 2^k, alors on termine en k étapes.
-
-   François a un principe d'induction généralisé qui permet de diviser par 2.
-*)
-let rec bsearch (t: int array) v i j =
+let rec bsearch (arr: int array) v i j =
   if j <= i then -1 else begin
     let m = i + (j - i) / 2 in
-    if v = t.(m) then
+    if v = arr.(m) then
       m
-    else if v < t.(m) then
-      bsearch t v i m
+    else if v < arr.(m) then
+      bsearch arr v i m
     else
-      bsearch t v (m+1) j
+      bsearch arr v (m+1) j
   end
